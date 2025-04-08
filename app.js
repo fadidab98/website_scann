fe49271c9330e4e9a771d928da38b690531457be
Fadidab <fadidab1998@gmail.com>
2025-04-08 16:24:21 +0200

    adding wildcard

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ScanController = require('./controllers/ScanController');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://*.serp24.online'],
+  origin: ['http://localhost:3000', 'https://fadilogic.serp24.online', 'https://serp24.online'],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
 }));
